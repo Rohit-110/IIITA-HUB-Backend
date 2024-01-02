d17dc645fe6b6751135e2a8dfac797c1c206c163
Rohit Pandey <iit2022019@iiita.ac.in>
2024-01-02 23:28:33 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
+import { config } from 'dotenv';
 
 export const connectDB = ()=>{
 
     mongoose.connect(process.env.MONGO_URI,{
         dbName: "SharkTank",
     })
-    
-    .then(() => console.log("Database Connected"))
+    .then((c) => console.log(`Database is Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
     
 
